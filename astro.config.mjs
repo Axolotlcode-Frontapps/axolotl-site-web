a0188aed1de4f038282b24b80e09fb2a940907fc
Luis Colunga Perez <perez.luis.colunga@gmail.com>
2025-02-17 01:19:38 -0600

    🔨 chore(config): add data for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,26 @@ import react from '@astrojs/react';
 import astroI18next from 'astro-i18next';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
+  site: 'https://www.axolotlcode.tech/',
   image: {
     service: passthroughImageService(),
   },
-  integrations: [react(), astroI18next(), tailwind({ nesting: true })],
+  integrations: [
+    react(),
+    astroI18next(),
+    tailwind({ nesting: true }),
+    sitemap({
+      canonicalURL: 'https://www.axolotlcode.tech/',
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en',
+          es: 'es',
+        },
+      },
+    }),
+  ],
 });
